4b4f8af954553fed77e9cb237c2031ceeb51c531
Cornelius Schumacher <schumacher@kde.org>
2011-05-22 22:21:28 +0200

    Fix variable name.

--- src/todoitem.cpp
+++ src/todoitem.cpp
@@ -29,17 +29,17 @@
 #include <KLocale>
 
 TodoItem::TodoItem( MainModel *model, MenuHandler *menuHandler,
-  const Bliss::Todo &identity )
-  : QObject( model ), m_model( model ), m_todo( identity ),
+  const Bliss::Todo &todo )
+  : QObject( model ), m_model( model ), m_todo( todo ),
     m_menuHandler( menuHandler ), m_edit( 0 ), m_editProxy( 0 )
 {
   init();
 }
 
 TodoItem::TodoItem( QGraphicsItem *item, MainModel *model,
-  const Bliss::Todo &identity )
+  const Bliss::Todo &todo )
   : QObject( model ), QGraphicsItemGroup( item ), m_model( model ),
-    m_todo( identity ), m_menuHandler( 0 ), m_edit( 0 ), m_editProxy( 0 )
+    m_todo( todo ), m_menuHandler( 0 ), m_edit( 0 ), m_editProxy( 0 )
 {
   init();
 }
